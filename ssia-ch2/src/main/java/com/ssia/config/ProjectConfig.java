7d17cdc792fd681923518e9a59e1ead558d5ad3a
Serhii Horbachov <serhii_horbachov@epam.com>
2023-08-07 15:37:26 +0300

    Chapter2.3.2: Applying authorization at the endpoint level.

--- ssia-ch2/src/main/java/com/ssia/config/ProjectConfig.java
+++ ssia-ch2/src/main/java/com/ssia/config/ProjectConfig.java
@@ -2,13 +2,21 @@ package com.ssia.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+
+/**
+ * In earlier versions of Spring Security, a security configuration class needed to extend a class named WebSecurityConfigurerAdapter.
+ * We don’t use this practice anymore today.
+ */
 @Configuration
 public class ProjectConfig {
 
@@ -29,8 +37,21 @@ public class ProjectConfig {
      * Because we overrode UserDetailsService, we also have to declare a PasswordEncoder.
      */
     @Bean
-    PasswordEncoder passwordEncoder(){
+    PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        //httpBasic() – Calling this method you instructed your app to accept HTTP Basic as an authentication method.
+        http.httpBasic(Customizer.withDefaults());//Customizer is a contract you implement to define the customization for either Spring Security element you configure.
+
+
+        //authorizeHttpRequests() – Calling this method you instructed the app how it should authorize the requests received
+        // on specific endpoints
+        http.authorizeHttpRequests(customizer -> customizer.anyRequest().permitAll());
+
+        return http.build();
     }
 
 
