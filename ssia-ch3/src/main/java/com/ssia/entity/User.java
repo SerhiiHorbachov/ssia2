ecd38f8144ae2fdb711e0a7f13b4b1876bd4dba9
Serhii <srg.gorbachov@gmail.com>
2023-08-11 09:15:53 +0300

    Chapter3.2.5: Multiple responsibilities related to user. Add User entity implements UserDetails.

---
+++ ssia-ch3/src/main/java/com/ssia/entity/User.java
@@ -0,0 +1,77 @@
+package com.ssia.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Entity
+public class User implements UserDetails {
+    @Id
+    private Long id;
+
+    private String username;
+    private String password;
+    private String authority;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
