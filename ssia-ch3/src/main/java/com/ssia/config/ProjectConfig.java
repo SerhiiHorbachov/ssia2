3fc41f60f97750110a9d193e03a1d683cb766ef5
Serhii <srg.gorbachov@gmail.com>
2023-08-15 11:03:21 +0300

    Chapter3.3.3: Use JdbcUserDetailsManager to access users

--- ssia-ch3/src/main/java/com/ssia/config/ProjectConfig.java
+++ ssia-ch3/src/main/java/com/ssia/config/ProjectConfig.java
@@ -1,25 +1,21 @@
 package com.ssia.config;
 
 
-import com.ssia.security.SecurityUser;
-import com.ssia.service.InMemoryUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import java.util.List;
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectConfig {
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails u = new SecurityUser("john", "12345", "read");
-        List<UserDetails> users = List.of(u);
-        return new InMemoryUserDetailsService(users);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
